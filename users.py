3584ac4dd74f9def0b8d78fdb15577fe2e211f71
Seyi Agboola <learnseyi@gmail.com>
2023-10-27 15:16:35 -0500

    updated users list

--- users.py
+++ users.py
@@ -1,6 +1,6 @@
 users = [
     {"id":3,"uname":"Adam","password": "org1234","role":"Admin"},
-    {"id":1,"uname":"Seyi","password": "student1234","role":"Student"},
-    {"id":2,"uname":"Tim","password": "admin1234","role":"Admin"}
+    {"id":1,"uname":"john","password": "student1234","role":"Student"},
+    {"id":2,"uname":"admin","password": "admin1234","role":"Admin"}
     
 ]
